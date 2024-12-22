6c3ab4a32f299e30d44a54663704514630c8e5ad
advay77 <advayanand37@gmail.com>
2024-12-23 02:40:26 +0530

    Added /welcome endpoint to server.js

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-
-
 app.use(express.static(path.join(__dirname)));
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -39,7 +35,6 @@ app.get('/api', (req, res) => {
     res.json({ message: 'Welcome to the API!' });
 });
 
-
 app.get('/api/products', (req, res) => {
     const products = [
         { id: 1, name: 'Product 1', price: 100 },
@@ -49,8 +44,12 @@ app.get('/api/products', (req, res) => {
     res.json(products);
 });
 
+// New welcome endpoint
+app.get('/welcome', (req, res) => {
+    res.json({ message: 'Welcome to the Flask API Service!' });
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
